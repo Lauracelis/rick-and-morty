a312829f82e975b76958e9eea7cdc89a53b94d74
Laura Celis <lauracelis436@gmail.com>
2022-12-13 14:43:30 -0500

    Feature: Configuraion and deployment the project

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -7,7 +7,6 @@ function NavPage({ page, setPage }) {
   return (
     <header className="d-flex justify-content-between align-items-center">
       <p>Page: {page}</p>
-
       <button
         className="btn btn-primary btn-sm"
         onClick={() => setPage(page + 1)}
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://Lauracelis.github.io/rick-and-morty",
   plugins: [react()],
 })
 
